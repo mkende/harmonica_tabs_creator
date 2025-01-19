43112cc8c3645e43a950073f4ff9d1949736d1e5
Mathias Kende <mathias@kende.fr>
2025-01-19 17:42:56 +0100

    Tidy, spelling and critic.

--- lib/Music/Harmonica/TabsCreator.pm
+++ lib/Music/Harmonica/TabsCreator.pm
@@ -10,17 +10,21 @@ use List::Util qw(min max);
 use Music::Harmonica::TabsCreator::NoteToToneConverter;
 use Readonly;
 
-our @EXPORT_OK = qw(sheet_to_tab);
+our $VERSION = '0.01';
+
+our @EXPORT_OK = qw(sheet_to_tab get_tuning);
 
 # Options to add:
 # - print B as H (international convention), but probably not Bb which stays Bb.
 
+Readonly my $TONES_PER_SCALE => 12;
+
 Readonly my %tunings => (
   # Written in the key of C to match the default key used in the note_to_tone
   # function.
-  richter_harmonica_no_bend => {
+  richter_no_bend => {
     tags => [qw(diatonic 10-holes)],
-    name => '10 holes diatonique Richter-tuned no bend',
+    name => 'Richter-tuned no bend',
     # We arbitrarily keep only +3 and never use -2.
     # We might need to change that if we wanted to support chords.
     tab => [qw(  1  -1 2   3 -3  4 -4 5  -5 6  -6 7  -7 8  -8 9  -9 10 -10)],
@@ -28,10 +32,14 @@ Readonly my %tunings => (
   },
   richter => {
     tags => [qw(diatonic 10-holes)],
-    name => '10 holes diatonique Richter-tuned with bending',
+    name => 'Richter-tuned with bending',
     # We arbitrarily keep only +3 and never use -2.
-    tab => [qw(  1  -1 -1' 2  -2' -2" 3  -3 -3' -3" -3"' 4  -4 -4' 5  -5 6  -6 -6' 7  -7 8  8'  -8 9  9'  -9 10 10' 10" -10)],
-    notes => [qw(C4 D4 Db4 E4 Gb4 F4  G4 B4 Bb4 A4  Ab4  C5 D5 Db5 E5 F5 G5 A5 Ab5 C6 B5 E6 Eb6 D6 G6 Gb6 F6 C7 B6  Bb6 A6)],
+    tab => [
+      qw(  1  -1 -1' 2  -2' -2" 3  -3 -3' -3" -3"' 4  -4 -4' 5  -5 6  -6 -6' 7  -7 8  8'  -8 9  9'  -9 10 10' 10" -10)
+    ],
+    notes => [
+      qw(C4 D4 Db4 E4 Gb4 F4  G4 B4 Bb4 A4  Ab4  C5 D5 Db5 E5 F5 G5 A5 Ab5 C6 B5 E6 Eb6 D6 G6 Gb6 F6 C7 B6  Bb6 A6)
+    ],
   },
 );
 
@@ -46,10 +54,14 @@ sub sheet_to_tab ($sheet) {
   while (my ($k, $v) = each %tunings) {
     my @matches = match_notes_to_tuning(\@tones, $v);
     for my $m (@matches) {
-      push @{$all_matches{$v->{name}}{$m->[1]}}, $m->[0];
+      push @{$all_matches{$k}{$m->[1]}}, $m->[0];
     }
   }
   return %all_matches;
+}
+
+sub get_tuning ($key) {
+  return %{$tunings{$key}}{qw(name tags)};
 }
 
 # Given all the tones (with C0 = 0) of a melody and the data of a given
@@ -59,20 +71,20 @@ sub match_notes_to_tuning ($tones, $tuning) {
   my $note_converter = Music::Harmonica::TabsCreator::NoteToToneConverter->new();
   my @scale_tones = map { $note_converter->convert($_) } @{$tuning->{notes}};
   my ($scale_min, $scale_max) = (min(@scale_tones), max(@scale_tones));
-  my ($tones_min, $tones_max) = (min(@$tones), max(@$tones));
-  my %scale_tones = map { $scale_tones[$_] => $tuning->{tab}[$_] } 0..$#scale_tones;
+  my ($tones_min, $tones_max) = (min(@{$tones}), max(@{$tones}));
+  my %scale_tones = map { $scale_tones[$_] => $tuning->{tab}[$_] } 0 .. $#scale_tones;
   my ($o_min, $o_max) = ($scale_min - $tones_min, $scale_max - $tones_max);
   my @matches;
   for my $o ($o_min .. $o_max) {
     my @tab = tab_from_tones($tones, $o, %scale_tones);
-    push @matches, [\@tab, $keys_offset[(12 - $o) % 12]] if @tab;
+    push @matches, [\@tab, $keys_offset[($TONES_PER_SCALE - $o) % $TONES_PER_SCALE]] if @tab;
   }
   return @matches;
 }
 
 sub tab_from_tones($tones, $offset, %scale_tones) {
   my @tab;
-  for my $t (@$tones) {
+  for my $t (@{$tones}) {
     return unless exists $scale_tones{$t + $offset};
     push @tab, $scale_tones{$t + $offset};
   }
@@ -89,10 +101,10 @@ __END__
 
 =head1 NAME
 
-Music::Harmonica::TabsConverter - Convert tabs to another format
+Music::Harmonica::TabsCreator - Convert tabs to another format
 
 =head1 SYNOPSIS
 
-  use Music::Harmonica::TabsConverter qw(convert);
+  use Music::Harmonica::TabsCreator qw(convert);
 
 =cut
--- lib/Music/Harmonica/TabsCreator/NoteToToneConverter.pm
+++ lib/Music/Harmonica/TabsCreator/NoteToToneConverter.pm
@@ -8,12 +8,14 @@ use utf8;
 use List::Util qw(any);
 use Readonly;
 
+our $VERSION = '0.01';
+
 # This class converts written note (accepting various syntax for the notes) into
 # tones (degrees) relative to the key of C4.
 
 sub new ($class, %options) {
   my $self = bless {
-    default_octave => $options{default_octave} // 5,
+    default_octave => $options{default_octave} // 5,  ## no critic (ProhibitMagicNumbers)
     key => $options{key} // 'C',
   }, $class;
   return $self;
@@ -26,7 +28,7 @@ Readonly my %NOTE_TO_TONE => (
   C => 0,
   Do => 0,
   D => 2,
-  Ré => 2,
+  'Ré' => 2,
   E => 4,
   Mi => 4,
   F => 5,
@@ -86,6 +88,11 @@ Readonly my %KEY_TO_ALTERATION => (
   'C#' => 7,
 );
 
+Readonly my $NOTE_NAME_RE => qr/ do|Do|ré|Ré|mi|Mi|fa|Fa|sol|Sol|la|La|si|Si | [A-H] /x;
+
+Readonly my $BASE_OCTAVE => 4;
+Readonly my $TONES_PER_SCALE => 12;
+
 sub note_to_tone ($note) {
   return $NOTE_TO_TONE{$note};
 }
@@ -93,9 +100,11 @@ sub note_to_tone ($note) {
 sub alteration_for_note ($self, $note) {
   my $alt = $KEY_TO_ALTERATION{$self->{key}};
   if ($alt > 0) {
-    return (any { note_to_tone($_) eq note_to_tone($note) } @SHARP_ORDER[0..$alt-1]) ? 1 : 0;
+    return (any { note_to_tone($_) eq note_to_tone($note) } @SHARP_ORDER[0 .. $alt - 1]) ? 1 : 0;
   } elsif ($alt < 0) {
-    return (any { note_to_tone($_) eq note_to_tone($note) } @FLAT_ORDER[0..abs($alt)-1]) ? -1 : 0;
+    return (any { note_to_tone($_) eq note_to_tone($note) } @FLAT_ORDER[0 .. abs($alt) - 1])
+        ? -1
+        : 0;
   } else {
     return 0;
   }
@@ -120,15 +129,18 @@ sub convert ($self, $symbols) {
     }
 
     # TODO: print only the relevant part of symbols here
-    if ($symbols =~ m/\G(do|Do|ré|Ré|mi|Mi|fa|Fa|sol|Sol|la|La|si|Si|[A-H])([#+b-]?)(-?\d+)?/gc) {
+    # There is a bug here that A-3 won’t be parsed as the - will be taken for a flat.
+    if ($symbols =~ m/\G ( ${NOTE_NAME_RE} ) ( [#+b-]? )( -?\d+ )?/xgc) {
       my ($note, $accidental, $octave) = (ucfirst($1), $2, $3 // $self->{default_octave});
-      my $base = 12 * ($octave - 4) + note_to_tone($note);
-      my $alteration = $accidental ? $ACCIDENTAL_TO_ALTERATION{$accidental} : $self->alteration_for_note($note);
+      my $base = $TONES_PER_SCALE * ($octave - $BASE_OCTAVE) + note_to_tone($note);
+      my $alteration =
+          $accidental ? $ACCIDENTAL_TO_ALTERATION{$accidental} : $self->alteration_for_note($note);
       push @out, $base + $alteration;
       next;
     }
 
-    die sprintf "Invalid syntax at position %d in: %s\n", pos($symbols), $symbols;
+    my $pos = pos($symbols);
+    die "Invalid syntax at position ${pos} in: ${symbols}\n";
   }
   return wantarray ? @out : \@out;
 }

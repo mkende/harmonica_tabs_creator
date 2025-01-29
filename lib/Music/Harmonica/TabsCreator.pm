37d8714387583357a1779b5bb70ea0545387a3ad
Mathias Kende <mathias@kende.fr>
2025-01-29 23:42:28 +0100

    Remove tabs transposed by one octave for a given key on chromatic harmonicas.
    This improves the situation a little but we still generates too many tabs for all keys.

--- lib/Music/Harmonica/TabsCreator.pm
+++ lib/Music/Harmonica/TabsCreator.pm
@@ -30,7 +30,8 @@ sub extend_chromatic_tuning ($tuning, $fix) {
     $tuning->{notes}[$i] =~ m/^(\w)(\d)$/ or die "Unexpected error\n";
     push @{$tuning->{notes}}, "${1}#${2}";
   }
-  @{$tuning->{bends}} = (1 x $size, 0 x $size);
+  @{$tuning->{bends}} = ((1) x ($size +1), (0) x ($size + 1));
+  $tuning->{max_offset} = $TONES_PER_SCALE - 1;
   if ($fix) {
     # Some brand use a D instead of a C (== B#) as the draw slide in, to give
     # one more note (instead of duplicating the C). It’s not really an issue if
@@ -189,6 +190,7 @@ sub generate_tunings ($max_bends, $tunings) {
         # TODO: this won’t work once we have chromatic harmonicas
         push @{$out{$k}{tabs}}, bend($tab, $b);
       }
+      $out{$k}{max_offset} = $v->{max_offset} if exists $v->{max_offset};
     }
   }
   return \%out;
@@ -266,7 +268,11 @@ sub match_notes_to_tuning ($tones, $tuning) {
   my ($o_min, $o_max) = ($scale_min - $tones_min, $scale_max - $tones_max);
   my @matches;
 
-  for my $o ($o_min .. $o_max) {
+  # max_offset is set for Chromatic harmonicas where to avoid generating the
+  # same thing over and over again, transposed by a full octave.
+  $o_max = min($o_max, $o_min + $tuning->{max_offset}) if exists $tuning->{max_offset};
+
+  for my $o ($o_min .. $o_max) { # (min($o_max, $o_min + $TONES_PER_SCALE - 1))) {
     my @tab = tab_from_tones($tones, $o, %scale_tones);
     push @matches, [\@tab, $KEYS_OFFSET[($TONES_PER_SCALE - $o) % $TONES_PER_SCALE]] if @tab;
   }
